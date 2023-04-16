df84621253b4810175f44590bf7b12388fb07638
Efendo <130039463+Efendo@users.noreply.github.com>
2023-04-16 14:13:50 +0200

    Add files via upload

---
+++ PYCBModl.py
@@ -0,0 +1,24 @@
+# This is the module version
+
+import random
+
+class RBN():
+    def __init__(self, bit, base):
+        self.bit = bit
+        self.base = base
+        self.num = ''
+        self.nums = set()
+        if self.base > 10 or self.base < 2:
+            raise SyntaxError('invalid Base')
+    
+    def GenNum(self):
+        self.num = ''
+        for i in range(0, self.bit):
+            self.num +=  str(random.randint(0, self.base-1))
+        return self.num
+    
+    def GenLis(self):
+        self.num = set()
+        while len(self.nums) < self.base**self.bit:
+            self.nums.add(self.GenNum())
+        return self.nums
