967185a2de05b0d5e35c87ddf0ef9b8e5643f9cb
Efendo <130039463+Efendo@users.noreply.github.com>
2023-04-18 15:49:58 +0200

    Update PYCBase.py

--- PYCBase.py
+++ PYCBase.py
@@ -1,22 +1,26 @@
-import random
+if __name__ != '__main__':
+    import random
 
-class RBN():
-    def __init__(self, bit, base):
-        self.bit = bit
-        self.base = base
-        self.num = ''
-        self.nums = set()
-        if self.base > 10 or self.base < 2:
-            raise SyntaxError('invalid Base')
+    class gen():
+        def __init__(self, bit, base):
+            self.bit = bit
+            self.base = base
+            self.num = ''
+            self.nums = set()
+            if self.base > 10 or self.base < 2:
+                raise SyntaxError('invalid Base')
     
-    def GenNum(self):
-        self.num = ''
-        for i in range(0, self.bit):
-            self.num +=  str(random.randint(0, self.base-1))
-        return self.num
+        def gint(self):
+            self.num = ''
+            for i in range(0, self.bit):
+               self.num +=  str(random.randint(0, self.base-1))
+            return self.num
     
-    def GenLis(self):
-        self.num = set()
-        while len(self.nums) < self.base**self.bit:
-            self.nums.add(self.GenNum())
-        return self.nums
+        def glis(self):
+            self.num = set()
+            while len(self.nums) < self.base**self.bit:
+                self.nums.add(self.GenNum())
+            return sorted(self.nums)
+
+else:
+    raise ImportError('to use this module import it into another script')
